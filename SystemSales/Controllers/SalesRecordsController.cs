9f3acef7ffaf4cb1859aa3044986c362ce49469c
Julio Oliveira <juliocesar.jc585@gmail.com>
2023-02-15 16:00:20 -0300

    Preparing sales search navigation views

---
+++ SystemSales/Controllers/SalesRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystemSales.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
